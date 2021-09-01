0dd60f3a1b701e2b6705f7d56017533ec8fd60d1
Boungjin Kim <rain1344@naver.com>
2021-09-01 15:47:23 +0900

    working: autoinstall script / todo: split config file to shell script

--- docker/container_test.sh
+++ docker/container_test.sh
@@ -2,18 +2,17 @@
 # require: docker image file, ml_model_file, configure_file, 
 # 윈도우에서 작업한 쉘 스크립트를 리눅스에서 실행할때 필요
 # sed -i -e 's/\r$//' [파일.sh]
+# sed -i -e 's/\r$//' container_test.sh
 CURRENT_PATH=$(pwd)
-PROJECT_PATH=/home/$USER/aizt
-GIT_URL='https://github.com/kbj804/xedm_pid_server.git'
-# 
-DOCKER_IMAGE="build_test"
-DOCKER_VERSION="0.8"
-DOCKER_PATH=/xedm_app
-CONTAINER_NAME="sTest"
-AIZT_PORT=8889
-# 
-CONFIG_NAME="conf.ini"
-ML_MODEL_NAME="auto_ml_model_0623.pkl"
+read_lines()
+{
+    while IFS = read -r line
+    do
+        echo $line
+    done <$1
+}
+
+eval $(read_lines) ${CURRENT_PATH}/config_for_install.txt
 # 
 # setting for dev
 # sudo docker build --tag ${DOCKER_IMAGE}:${DOCKER_VERSION} .
@@ -24,18 +23,20 @@ ML_MODEL_NAME="auto_ml_model_0623.pkl"
 # 
 # docker containser start
 # sudo docker restart $CONTAINER_NAME
-sudo docker run --name ${CONTAINER_NAME} -p ${AIZT_PORT} ${DOCKER_IMAGE}:${DOCKER_VERSION}
+sudo docker stop ${CONTAINER_NAME}
+sudo docker rm ${CONTAINER_NAME}
+sudo docker run -d --restart always --name ${CONTAINER_NAME} -p ${AIZT_PORT}:${DOCKER_PORT} ${DOCKER_IMAGE}:${DOCKER_VERSION}
 # 
 # make working directory in docker
-sudo docker exec ${CONTAINER_NAME} mkdir ${DOCKER_PATH}
+# sudo docker exec ${CONTAINER_NAME} mkdir ${DOCKER_PATH}
 # 
 # input project source code
 sudo docker cp ${PROJECT_PATH} ${CONTAINER_NAME}:${DOCKER_PATH}
 # 
 # setting config file
-sudo docker cp ${CURRENT_PATH}/${CONFIG_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/fastapp/common/${CONFIG_NAME}
-sudo docker cp ${CURRENT_PATH}/${ML_MODEL_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/fastapp/data/results/ml_model/${ML_MODEL_NAME}
+sudo docker cp ${CURRENT_PATH}/${CONFIG_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/common/${CONFIG_NAME}
+sudo docker cp ${CURRENT_PATH}/${ML_MODEL_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/data/results/ml_model/${ML_MODEL_NAME}
 # 
 #
 # start
-sudo docker exec ${CONTAINER_NAME} python3 ${DOCKER_PATH}/fastapp/main.py
+sudo docker exec ${CONTAINER_NAME} python3 ${DOCKER_PATH}/aizt/fastapp/main.py
