bbde10af1bde8e965666bb951d416ab782b4df54
Boungjin Kim <rain1344@naver.com>
2021-09-16 16:30:03 +0900

    update script for using airflow / TODO: modify(dbtest.sh) airflow.cfg in ml_container

--- docker/database/dbtest.sh
+++ docker/database/dbtest.sh
@@ -36,30 +36,32 @@ sudo git clone ${GIT_URL} ${PROJECT_PATH}
 # 
 # docker containser start
 # sudo docker restart $CONTAINER_NAME
-echo "Docker container reset & restart ..."
-sudo docker stop ${CONTAINER_NAME}
-sudo docker rm ${CONTAINER_NAME}
-# sudo docker run -d --restart always --name ${CONTAINER_NAME} -p ${AIZT_PORT}:${DOCKER_PORT} ${DOCKER_IMAGE}:${DOCKER_VERSION}
+echo "### Docker container reset & restart ..."
+sudo docker stop ${ML_CONTAINER_NAME}
+sudo docker rm ${ML_CONTAINER_NAME}
+# sudo docker run -d --restart always --name ${ML_CONTAINER_NAME} -p ${AIZT_PORT}:${DOCKER_PORT} ${DOCKER_IMAGE}:${DOCKER_VERSION}
 docker-compose --env-file ${CURRENT_PATH}/.env up -d
 # 
-# make working directory in docker
-# sudo docker exec ${CONTAINER_NAME} mkdir ${DOCKER_PATH}
 # 
 # input project source code
-echo "Downloading XEDM ML Server source code ..."
-sudo docker cp ${PROJECT_PATH} ${CONTAINER_NAME}:${DOCKER_PATH}
+echo "### Downloading XEDM ML Server source code ..."
+sudo docker cp ${PROJECT_PATH} ${ML_CONTAINER_NAME}:${DOCKER_PATH}
 # 
-echo "Setting app and ml config files ..."
-sudo docker cp ${CURRENT_PATH}/${CONFIG_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/common/${CONFIG_NAME}
-sudo docker cp ${CURRENT_PATH}/${ML_MODEL_NAME} ${CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/data/results/ml_model/${ML_MODEL_NAME}
+echo "### Setting app and ml config files ..."
+# make working directory in docker
+sudo docker exec ${ML_CONTAINER_NAME} mkdir -p ${DOCKER_PATH}/aizt/fastapp/data/results/ml_model
+sudo docker cp ${CURRENT_PATH}/${CONFIG_NAME} ${ML_CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/common/${CONFIG_NAME}
+sudo docker cp ${CURRENT_PATH}/${ML_MODEL_NAME} ${ML_CONTAINER_NAME}:${DOCKER_PATH}/aizt/fastapp/data/results/ml_model/${ML_MODEL_NAME}
 # 
+echo "### Setting Airflow DDL on DB ..."
+sudo docker exec ${DB_CONTAINER_NAME} # TODO
 # 
-echo "Starting XEDM ML Server  ..."
-sudo docker exec ${CONTAINER_NAME} python3 ${DOCKER_PATH}/aizt/fastapp/main.py
+echo "### Starting XEDM ML Server  ..."
+sudo docker exec ${ML_CONTAINER_NAME} python3 ${DOCKER_PATH}/aizt/fastapp/main.py
 #
 #
-echo "Databases ENV Setting & "
-PORT_FORWARD=$(awk '/^\[DB]/{f=1} f==1&&/^PORT/{print $3;exit}' ${CONFIG_FILE})
-DB_USER=$(awk '/^\[DB]/{f=1} f==1&&/^USER/{print $3;exit}' ${CONFIG_FILE})
-DB_PW=$(awk '/^\[DB]/{f=1} f==1&&/^USER/{print $3;exit}' ${CONFIG_FILE})
-DB_NAME=$(awk '/^\[DB]/{f=1} f==1&&/^NAME/{print $3;exit}' ${CONFIG_FILE})
+# echo "Databases ENV Setting & "
+# PORT_FORWARD=$(awk '/^\[DB]/{f=1} f==1&&/^PORT/{print $3;exit}' ${CONFIG_FILE})
+# DB_USER=$(awk '/^\[DB]/{f=1} f==1&&/^USER/{print $3;exit}' ${CONFIG_FILE})
+# DB_PW=$(awk '/^\[DB]/{f=1} f==1&&/^USER/{print $3;exit}' ${CONFIG_FILE})
+# DB_NAME=$(awk '/^\[DB]/{f=1} f==1&&/^NAME/{print $3;exit}' ${CONFIG_FILE})
--- docker/readme.md
+++ docker/readme.md
@@ -24,3 +24,26 @@
 > - airflow db init
 > - airflow users create -r Admin -u kbj -e kbj804@inzent.com -f Boungjin -l Kim -p 1234
 > - airflow webserver
+
+- Script for creating postgresql DB, Table
+```
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  \connect $APP_DB_NAME $APP_DB_USER
+  BEGIN;
+    CREATE TABLE IF NOT EXISTS event (
+	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	  event_data JSON NOT NULL,
+	  version INT,
+	  UNIQUE(aggregate_id, version)
+	);
+	CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
+  COMMIT;
+EOSQL
+```
